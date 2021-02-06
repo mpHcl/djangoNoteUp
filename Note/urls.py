95e30cbc6458424e151a2e5e309047e270f82498
Michał Pilch <m.pilch2002@gmail.com>
2021-02-06 12:58:19 +0100

    Log and register
    I've created register page, and improved login page style

--- Note/urls.py
+++ Note/urls.py
@@ -11,4 +11,9 @@ urlpatterns = [
     path("login", views.login_view, name='login_page'),
     path("post_note", views.post_note, name='post_note'),
     path("new_note", views.post_page, name='new_note'),
+    path("serach_note", views.search_note, name='search_note'),
+    path("edit_page/<note_id>", views.edit_page, name='edit_page'),
+    path("edit_note", views.edit_note, name='edit_note'),
+    path("register", views.register_view, name='register_page'),
+    path("register_account", views.register_account, name='register_account')
 ]
--- Note/views.py
+++ Note/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 
 from .models import Note
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import authenticate, logout, login
 from django.http import Http404
+from Note.email_check import check
 
 
 # Create your views here.
@@ -64,6 +65,33 @@ def login_validate(request):
         return render(request, 'Note/login.html', context={'message': 'Bad username or password'})
 
 
+def register_view(request):
+    if request.user.is_authenticated:
+        logout(request)
+    return render(request, 'Note/register.html')
+
+
+def register_account(request):
+    username = request.POST['username']
+    password = request.POST['password']
+    email = request.POST['email']
+    first_name = request.POST['first_name']
+    last_name = request.POST['last_name']
+
+    if email and username and password and first_name and last_name:
+        if not check(email):
+            return render(request, "Note/register.html", context={'email_message': "Invalid email :/"})
+        user = User.objects.create_user(username, email, password)
+        user.first_name = first_name
+        user.last_name = last_name
+        user.save()
+        logout(request)
+        login(request, user)
+        return redirect('Note:index')
+    else:
+        return render(request, "Note/register.html", context={'message': "Missing some information :/"})
+
+
 def post_note(request):
     if request.user.is_authenticated:
         user = User.objects.get(pk=request.user.id)
@@ -72,12 +100,57 @@ def post_note(request):
         category = request.POST['category']
         mod_date = timezone.now()
 
-        note = Note.objects.create(user=user, title=title, text=text, category=category, modification_date=mod_date)
+        if title and text and category:
+            note = Note.objects.create(user=user, title=title, text=text, category=category, modification_date=mod_date)
+            return redirect('Note:details', note_id=note.id)
+        else:
+            return redirect('Note:new_note')
+    else:
+        return redirect('Note:login_page')
 
-        return redirect('Note:details', note_id=note.id)
+
+def edit_note(request):
+    if request.user.is_authenticated:
+        user = User.objects.get(pk=request.user.id)
+        note_id = request.POST['id']
+        title = request.POST['title']
+        text = request.POST['text']
+        category = request.POST['category']
+        mod_date = timezone.now()
+
+        note = get_object_or_404(Note, user=user, pk=note_id)
+        if title and text and category:
+            note.title = title
+            note.category = category
+            note.modification_date = mod_date
+            note.text = text
+            note.save()
+            return redirect('Note:details', note_id=note.id)
+        else:
+            return redirect('Note:new_note')
     else:
         return redirect('Note:login_page')
 
 
 def post_page(request):
     return render(request, 'Note/new_note.html')
+
+
+def edit_page(request, note_id):
+    if request.user.is_authenticated:
+        note = get_object_or_404(Note, pk=note_id, user=request.user)
+        return render(request, 'Note/new_note.html', context={'note': note, 'edit': True})
+    else:
+        return redirect('Note:login_page')
+
+
+def search_note(request):
+    if request.user.is_authenticated:
+        search_value = request.POST['value']
+        notes = Note.objects.filter(user=request.user, title__contains=search_value).order_by('-modification_date')
+        categories_list = notes.values_list('category', flat=True).distinct().order_by()
+        return render(request, 'Note/index.html', context={
+            'user': request.user, 'notes': notes, 'categories': categories_list
+        })
+    else:
+        return redirect('Note:login_page')
